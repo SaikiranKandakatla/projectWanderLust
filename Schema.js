0c3b682f8e8c8f468517c3a3d8d1e32ea7e2406d
SaikiranKandakatla <saikiran.kandakatla69@gmail.com>
2025-04-25 16:05:49 +0530

    added utils Schema.js and Error.js

---
+++ Schema.js
@@ -0,0 +1,11 @@
+let joi=require("joi");
+module.exports.ListingSchema=joi.object({
+    listing:joi.object({
+        title:joi.string().required(),
+        decription:joi.string().required(),
+        location:joi.string().required(),
+        country:joi.string().required(),
+        price:joi.number().required().min(0),
+        image:joi.string().allow("",null)
+    }).required()
+})
--- app.js
+++ app.js
@@ -4,8 +4,12 @@ let ejsMate=require("ejs-mate");
 app.engine("ejs",ejsMate);
 let mongoose=require("mongoose");
 let mo=require("method-override");
+const ListingSchema=require("./Schema.js");
+const wrapAsync=require("./utils/WrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js");
 app.use(mo("_method"));
 let path=require("path");
+app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}))
 app.set("view engine","ejs");
@@ -22,53 +26,84 @@ async function main(){
     await mongoose.connect(Mongo_Url);
 }
 
-//backend connection
-app.listen(8080,()=>{
-    console.log("The server is running at 8080");
-});
+let ValidateListing=(req,res,next)=>{
+    let {error}=ListingSchema.validate(req.body);
+    if(error){
+        let ErrMsg=error.details.map(el=>el.message).join(",");
+        throw new ExpressError(400,ErrMsg);
+    }else{
+        next();
+    }
+}
+
+
+// //home root
+// app.get("/",(req,res)=>{
+//     res.send("This is root");
+// });
 //home Page
-app.get("/listings",async(req,res)=>{
+app.get("/listings",wrapAsync(async(req,res)=>{
     let listin=await listings.find({});
     res.render("listings/index.ejs",{listin});
-});
+}));
 
 //create new page
-app.get("/listings/new",async(req,res)=>{
+app.get("/listings/new",wrapAsync(async(req,res)=>{
     res.render("listings/create.ejs");
+}));
+app.all(/(.*)/, (req,res,next)=>{
+    next(new ExpressError(404,"Page Not Found!!"));
 });
-//show page
-app.get("/listings/:id",async(req,res)=>{
+
+app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let listin=await listings.findById(id);
     res.render("listings/show.ejs",{listin});
-});
-//create new page
-app.post("/listings",(req,res)=>{
-    let listin=req.body.listing;
-    let user=new listings(listin)
-    user.save();
-    res.redirect("/listings");
-});
+}));
 //editing
-app.get("/listings/:id/edit",async(req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let listin=await listings.findById(id);
     res.render("listings/edit.ejs",{listin});
-});
+}));
 
-app.put("/listings/:id",async(req,res)=>{
+app.put("/listings/:id",ValidateListing,wrapAsync(async(req,res)=>{
     let {id}=req.params;
     await listings.findByIdAndUpdate(id,req.body.listing);
     res.redirect("/listings");
-});
+}));
 // delete 
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let listin=await listings.findByIdAndDelete(id);
     console.log(listin);
     res.redirect("/listings");
+}));
+//create new page
+app.post("/listings",ValidateListing,wrapAsync(async(req,res,next)=>{
+    let listin=req.body.listing;
+    let user=await new listings(listin)
+    await user.save();
+    res.redirect("/listings");
+    
+}));
+//show page
+app.all(/(.*)/, (req,res,next)=>{
+    next(new ExpressError(404,"Page Not Found!!"));
 });
-//home root
-app.get("/",(req,res)=>{
-    res.send("This is root");
+app.use((req,res,next)=>{
+    req.time=new Date(Date.now());
+    console.log(req.hostname,req.method,req.time);
+})
+// if it is not matches with any route we need to show page not found
+
+//errorHandler
+app.use((err,req,res,next)=>{
+    let{statusCode=500,message="Something Went Wrong"}=err;
+    res.status(statusCode).render("Error.ejs",{err});
+})
+//backend connection
+app.listen(8080,()=>{
+    console.log("The server is running at 8080");
 });
+
